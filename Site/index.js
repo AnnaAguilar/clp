09cd60db3c139e3099854f9d3472bf81cfada6f9
AnnaAguilar <34386951+AnnaAguilar@users.noreply.github.com>
2018-12-06 11:51:47 -0200

    Update index.js

--- Site/index.js
+++ Site/index.js
@@ -6,7 +6,13 @@ let desafios = [
   "Jogando Conversa fora no escritório quem tem mais colegas de trabalho? (Demanda: Trendy Skills)",
   "Hora do Aumento! Seu chefe lhe deu 6 meses para aprender uma nova linguagem de programação (Salário)",
   "Dia dos velhinhos quem nasceu primeiro? (Lançamento: Original)",
-  "Git add, Git Commit, Git Push! (Lançamento: Mais Recente)"
+  "Git add, Git Commit, Git Push! (Lançamento: Mais Recente)", 
+  "Seu dia de sorte, essa rodada vale o dobro (Descarte uma carta)",
+  "Os últimos seram os primeiros, quem ta perdendo escolhe (Atributo escolhido pelo jogador com menos pontos)",
+  "Bolsa Família (Salário: Mais baixo)",
+  "Quem quer ser um milionario? (Salário)", 
+  "Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah! (Descarte 2 cartas)"
+  
 ];
 
 function generate() {
